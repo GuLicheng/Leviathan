959e313249bf86b7a3eea4d9589f869299822fea
GuLicheng <576356467@qq.com>
2021-01-20 18:24:52 +0800

    update test for tuple_extend

--- test/src/tuple_test.cpp
+++ test/src/tuple_test.cpp
@@ -6,18 +6,26 @@
 
 using namespace leviathan;
 
+constexpr int add1(int a, int b)
+{
+    return a + b;
+}
+
+int add2(int a, int b)
+{
+    return a + b;
+}
 
 int main()
 {
-    constexpr auto t1 = std::make_tuple(1, 2, 3, 4, 5);
+    auto f = ::add2;
+    int x;
+    std::cin >> x;
+    auto t1 = std::make_tuple(1, 2, 3, 4, 5, 6, x);
     leviathan::print_tuple(std::cout, t1);
     std::cout << std::endl;
-    constexpr auto t2 = reverse_tuple_by_copy(t1);
+    auto t2 = reverse_tuple_by_copy(t1);
     leviathan::print_tuple(std::cout, t2);
     std::cout << std::endl;
-    // auto t3 = reverse_tuple_by_move(t1);
-    // leviathan::print_tuple(std::cout, t3);
-    static_assert(leviathan::
-        tuple_inner_preduct(t1, t2, std::plus<>(), std::multiplies<>()) == 6*6*6*6*6);
-    // std::cout << res << std::endl;
+    std::cout << leviathan::tuple_inner_preduct(t1, t2, ::add2, ::add2) << std::endl;
 }
