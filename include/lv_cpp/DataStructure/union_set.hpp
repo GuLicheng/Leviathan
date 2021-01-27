126afebe95e1a7b9d70690d90fbaad062baef64e
GuLicheng <576356467@qq.com>
2021-01-27 13:38:36 +0800

    update union_set

--- include/lv_cpp/DataStructure/union_set.hpp
+++ include/lv_cpp/DataStructure/union_set.hpp
@@ -1,17 +1,24 @@
+/*
+    https://pintia.cn/problem-sets/994805342720868352/problems/994805354108403712
+*/
+
 #ifndef __UNION_SET_HPP__
 #define __UNION_SET_HPP__
 
+#include <iostream>
+
+// for x = father[node], node is the number of this cluster if x < 0, otherwise root of node
 class union_set
 {
 public:
-    union_set() 
+    union_set(int max_size) : father{new int[max_size]}, size{max_size} 
     {
-        for(int i = 0; i < N; ++i) father[i] = i;
+        for (int i = 0; i <= max_size; ++i) father[i] = -1;
     }
 
-    int find(int x)
+    int find(int x) noexcept
     {
-        return x == father[x] ? x : father[x] = find(father[x]);//路径压缩
+        return father[x] < 0 ? x : father[x] = find(father[x]);//路径压缩
     }
 
     bool check(int x, int y) 
@@ -19,20 +26,60 @@ public:
         return find(x) == find(y); 
     }
 
-    void merge(int x, int y) 
+    bool merge(int x, int y) 
     {
-        if(!check(x, y)) father[father[x]] = father[y];
+        int fx = find(x);
+        int fy = find(y);
+        if (fx == fy)
+            return false;
+        // make smaller root
+        else if (fx < fy)
+        {
+            father[fx] += father[fy];
+            father[fy] = fx;
+        }
+        else
+        {
+            father[fy] += father[fx];
+            father[fx] = fy;
+        }
+        return true;
+    }
+
+    int count(int l = 0) const noexcept
+    {
+        int res = 0;
+        for (int i = l; i < size; ++i)
+            if (father[i] < 0)
+                res ++;
+        return res;
     }
 
     // for extend
-    int* data() 
+    int* data() noexcept
     { 
         return father; 
     }
 
+    ~union_set() 
+    {
+        delete father;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const union_set& s1)
+    {
+        os << '[';
+        for (int i = 0; i < s1.size; ++i)
+        {
+            if (i) std::cout << ',' << ' ';
+            std::cout << s1.father[i];
+        }
+        return os << ']';
+    }
+
 private:
-    constexpr static int N = 10000;
-    int father[N];
+    int size;
+    int* father;
 };
 
 #endif
--- test/src/INI.cpp
+++ test/src/INI.cpp
@@ -1,24 +1,28 @@
-#include "INI.hpp"
+#include <lv_cpp/INI.hpp>
+#include <functional>
 
-
-namespace leviathan::INI
-{
-
-
-
-}  // namespace leviathan
+constexpr const char* path = "./data.ini";
 
 int main()
 {
-    using namespace leviathan::INI;
-    INI_handler ini;
-    ini.load("./rulesmo.ini");
-    ini.show();
-    std::cout << "lines : " << ini.lines << std::endl;
-    std::cout << "Test successfully\n";
+    // std::function<void(int, int)> a = std::swap<int, int>;
+    leviathan::INI::INI_handler reader;
+    reader.load(path);
+    // reader.show();
+    for (auto&& key : reader.get_sections())
+    {
+        std::cout << key << ',';
+    }
+    std::cout << "\n=================================" << std::endl;
+    for (auto&& value : reader.get_entries())
+    {
+        std::cout << value << ',';
+    }
+    std::cout << "=================================" << std::endl;
+    for (auto&& [k, v] : reader.get_items())
+    {
+        std::cout << "key is" << k << "and value is " << v << std::endl;
+    }
+    std::cout << "=================================" << std::endl;
+    std::cout << "Ok\n";
 }
-
-
-
-
-
