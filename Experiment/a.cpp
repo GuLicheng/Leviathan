f30f7cd7226605a9e62cebc177102718eb5c1a24
GuLicheng <576356467@qq.com>
2021-12-13 21:52:36 +0800

    Update

--- Experiment/a.cpp
+++ Experiment/a.cpp
@@ -3,40 +3,22 @@
 #include <ranges>
 #include <vector>
 #include <iostream>
-
+#include <stdio.h>
 #include <lv_cpp/math/algorithm.hpp>
+#include <lv_cpp/meta/function_traits.hpp>
 
-struct merge_sort_fn
-{
-    template <std::random_access_iterator I, std::sentinel_for<I> S, 
-        typename Comp = std::ranges::less, typename Proj = std::identity>
-    requires std::sortable<I, Comp, Proj>
-    constexpr I operator()(I first, S last, Comp comp = {}, Proj proj = {}) const
-    {
-        if (last - first > 1)
-        {
-            auto middle = first + (last - first) / 2;
-            (*this)(first, middle, std::ref(comp), std::ref(proj));
-            (*this)(middle, last, std::ref(comp), std::ref(proj));
-            std::ranges::inplace_merge(first, middle, last, std::ref(comp), std::ref(proj));
-        }
-        return first;
-    }
+template <typename F>
+struct T : std::type_identity<bool> { };
 
-    template <std::ranges::random_access_range Range, typename Comp = std::ranges::less, typename Proj = std::identity>
-    requires std::sortable<std::ranges::iterator_t<Range>, Comp, Proj>
-    constexpr std::ranges::borrowed_iterator_t<Range> operator()(Range&& r, Comp comp = {}, Proj proj = {}) const
-    {
-        return (*this)(std::ranges::begin(r), std::ranges::end(r), std::move(comp), std::move(proj));
-    }
-};
+template <typename Res, typename... Args>
+struct T<Res(*)(Args......)> : std::type_identity<Res> { };
 
-inline constexpr merge_sort_fn merge_sort{ };
+void f(int) { }
 
 int main()
 {
-    std::vector arr{4, 5, 6, 1, 2, 3};
-    merge_sort(arr);
-    std::ranges::copy(arr, std::ostream_iterator<int>{std::cout, " "});
+    using T = decltype(::printf);
+    typename leviathan::meta::function_traits<T>::nth_arg<0> i = "Hello";
+    ::printf(i);
 }
 
