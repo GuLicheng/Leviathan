6193aaa91419b139e5643d20098c5e13c4662183
GuLicheng <576356467@qq.com>
2021-12-14 18:00:23 +0800

    Update

--- Experiment/a.cpp
+++ Experiment/a.cpp
@@ -7,18 +7,8 @@
 #include <lv_cpp/math/algorithm.hpp>
 #include <lv_cpp/meta/function_traits.hpp>
 
-template <typename F>
-struct T : std::type_identity<bool> { };
-
-template <typename Res, typename... Args>
-struct T<Res(*)(Args......)> : std::type_identity<Res> { };
-
-void f(int) { }
 
 int main()
 {
-    using T = decltype(::printf);
-    typename leviathan::meta::function_traits<T>::nth_arg<0> i = "Hello";
-    ::printf(i);
 }
 
