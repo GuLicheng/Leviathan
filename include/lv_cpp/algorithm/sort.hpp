f1970a39011eb4ee2ec83acf49d1d48b016c908c
GuLicheng <576356467@qq.com>
2021-12-25 00:46:11 +0800

    Update

--- include/lv_cpp/algorithm/sort.hpp
+++ include/lv_cpp/algorithm/sort.hpp
@@ -1,89 +1,72 @@
 #ifndef __ALGORITHM_HPP__
 #define __ALGORITHM_HPP__
 
-#include <algorithm>
-#include <vector>
-#include <concepts>
+#include <algorithm>    // some base algorithm
+#include <vector>       // buffer for some algorithm
+#include <functional>   // std::invoke
+#include <concepts>     // some concepts such as sortable and random_access_iterator
 
-namespace leviathan
+
+namespace leviathan::sort
 {
-
-    // Proj and pass by value, std::move will make algorithm very slowly
-    struct insertion_sort_fn
+    // Copy from stl_algo.h
+    template <typename Comp, typename Proj>
+    constexpr auto make_comp_proj(Comp& comp, Proj& proj)
     {
-        // for simplifier, use random_access_iterator 
-        template <std::random_access_iterator I, std::sentinel_for<I> S, 
-                    typename Comp = std::ranges::less, typename Proj = std::identity>
-        requires std::sortable<I, Comp, Proj>
-        constexpr I operator()(I first, S last, Comp comp = {}, Proj proj = {}) const
+        return [&](auto&& lhs, auto&& rhs) -> bool
         {
-            if (first == last)
-                return first;
-
-            auto i = first + 1;
-            for (; i != last; ++i)
-            {
-                auto j = i - 1;
-                // if arr[j] <= arr[i] continue
-                if (!std::invoke(comp, std::invoke(proj, *i), std::invoke(proj, *j))) continue;
-                else
-                {
-                    // auto pos = std::ranges::upper_bound(first, i, *i, std::ref(comp), std::ref(proj));
-                    auto pos = std::ranges::upper_bound(first, i, *i, comp, proj);
-                    auto tmp = std::move(*i);
-                    std::ranges::move(pos, i, pos + 1);
-                    *pos = std::move(tmp);
-                } 
-            }
-            return i;
-        }
-
-        template <std::ranges::random_access_range Range, typename Comp = std::ranges::less, typename Proj = std::identity>
-        requires std::sortable<std::ranges::iterator_t<Range>, Comp, Proj>
-        constexpr std::ranges::borrowed_iterator_t<Range>
-        operator()(Range&& r, Comp comp = {}, Proj proj = {}) const
-        {
-            return (*this)(std::ranges::begin(r), std::ranges::end(r), std::move(comp), std::move(proj));
-        }
-    };
-
-    inline constexpr insertion_sort_fn insertion_sort{ };
+            using _TL = decltype(lhs);
+            using _TR = decltype(rhs);
+            return std::invoke(comp,
+                                 std::invoke(proj, std::forward<_TL>(lhs)),
+                                 std::invoke(proj, std::forward<_TR>(rhs)));
+        };
+    }
 
 
-    struct merge_sort_fn
+    template <typename I, typename S,  typename Comp = std::less<>>
+    constexpr I insertion_sort(I first, S last, Comp comp = {}) 
     {
-        template <std::random_access_iterator I, std::sentinel_for<I> S, 
-            typename Comp = std::ranges::less, typename Proj = std::identity>
-        requires std::sortable<I, Comp, Proj>
-        constexpr I operator()(I first, S last, Comp comp = {}, Proj proj = {}) const
-        {
-            if (last - first > 1)
-            {
-                auto middle = first + (last - first) / 2;
-                (*this)(first, middle, comp, proj);
-                (*this)(middle, last, comp, proj);
-                std::ranges::inplace_merge(first, middle, last, comp, proj);
-            }
+        if (first == last)
             return first;
-        }
 
-        template <std::ranges::random_access_range Range, typename Comp = std::ranges::less, typename Proj = std::identity>
-        requires std::sortable<std::ranges::iterator_t<Range>, Comp, Proj>
-        constexpr std::ranges::borrowed_iterator_t<Range> operator()(Range&& r, Comp comp = {}, Proj proj = {}) const
+        auto i = first + 1;
+        for (; i != last; ++i)
         {
-            return (*this)(std::ranges::begin(r), std::ranges::end(r), std::move(comp), std::move(proj));
+            auto j = i - 1;
+            // if arr[j] <= arr[i] continue
+            if (!comp(*i, *j)) continue;
+            else
+            {
+                auto pos = std::upper_bound(first, i, *i, comp);
+                auto tmp = std::move(*i);
+                std::move(pos, i, pos + 1);
+                *pos = std::move(tmp);
+            } 
         }
-    };
+        return i;
+    }
 
-    inline constexpr merge_sort_fn merge_sort{ };
-
-
-    struct tim_sort_fn
+    template <typename I, typename S,  typename Comp = std::less<>>
+    constexpr I merge_sort(I first, S last, Comp comp = {})
     {
-        constexpr static int MinSize = 32;
+        if (last - first > 1)
+        {
+            auto middle = first + (last - first) / 2;
+            merge_sort(first, middle, comp);
+            merge_sort(middle, last, comp);
+            std::inplace_merge(first, middle, last, comp);
+        }
+        return first;
+    }
 
-        template <typename I, typename S, typename Comp, typename Proj>
-        constexpr static I count_run_and_make_ascending(I first, S last, Comp comp, Proj proj) 
+    // for TimSort
+    namespace detail
+    {
+        inline constexpr int MinSize = 32; 
+
+        template <typename I, typename S, typename Comp>
+        constexpr I count_run_and_make_ascending(I first, S last, Comp comp) 
         {
             // comp is less relationship
             if (first == last)
@@ -95,28 +78,27 @@ namespace leviathan
             if (right == last)
                 return right;
 
-            if (std::invoke(comp, std::invoke(proj, *right), std::invoke(proj, *left)))
+            if (comp(*right, *left))
             {
                 // (first > last  <=>  last < first ) ++ and reverse
                 do { ++left; ++right; } 
-                while (right != last && std::invoke(comp, std::invoke(proj, *right), std::invoke(proj, *left)));
-                std::ranges::reverse(first, right);
+                while (right != last && comp(*right, *left));
+                std::reverse(first, right);
             }
             else
             {
                 // while first <= last ++
                 do { ++left; ++right; } 
-                while (right != last && !std::invoke(comp, std::invoke(proj, *right), std::invoke(proj, *left)));
+                while (right != last && !comp(*right, *left));
             }
-
             return right;
         }
 
         template <typename T>
-        constexpr static T min_run_length(T n) 
+        constexpr T min_run_length(T n) 
         {
             T r = 0;      
-            while (n >= MinSize) 
+            while (n >= detail::MinSize) 
             {
                 r |= (n & 1);
                 n >>= 1;
@@ -124,8 +106,8 @@ namespace leviathan
             return n + r;
         }
 
-        template <typename T, typename Comp, typename Proj>
-        constexpr static void merge_collapse(std::vector<T>& runs, Comp comp, Proj proj)
+        template <typename T, typename Comp>
+        constexpr void merge_collapse(std::vector<T>& runs, Comp comp)
         {
             while (runs.size() > 2)
             {
@@ -137,7 +119,7 @@ namespace leviathan
                     auto right = runs[2];
                     if (middle - left <= right - middle)
                     {
-                        std::ranges::inplace_merge(left, middle, right, comp, proj);
+                        std::inplace_merge(left, middle, right, comp);
                         runs.erase(runs.begin() + 1); // remove middle
                     }
                     else 
@@ -159,12 +141,12 @@ namespace leviathan
                     {
                         if (Z < X) // merge first 2
                         {
-                            std::ranges::inplace_merge(iter1, iter2, iter3, comp, proj);
+                            std::inplace_merge(iter1, iter2, iter3, comp);
                             runs.erase(last_pos - 3);
                         }
                         else // merge last 2
                         {
-                            std::ranges::inplace_merge(iter2, iter3, iter4, comp, proj);
+                            std::inplace_merge(iter2, iter3, iter4, comp);
                             runs.erase(last_pos - 2);
                         }
                     }
@@ -172,109 +154,166 @@ namespace leviathan
             }
         }
 
-        template <typename T, typename Comp, typename Proj>
-        constexpr static void merge_force_collapse(std::vector<T>& runs, Comp comp, Proj proj)
+        template <typename T, typename Comp>
+        constexpr void merge_force_collapse(std::vector<T>& runs, Comp comp)
         {
-            const int sz = static_cast<int>(runs.size());
+            const int sz = static_cast<int>(runs.size()) - 1;
             if (sz > 2)
             {
-                for (int i = 1; i + 1 < sz; ++i)
-                    std::ranges::inplace_merge(runs[0], runs[i], runs[i + 1], comp, proj);
+                for (int i = 1; i < sz; ++i)
+                    std::inplace_merge(runs[0], runs[i], runs[i + 1], comp);
             }
         }
-    
-    public:
-        template <std::random_access_iterator I, std::sentinel_for<I> S, 
-            typename Comp = std::ranges::less, typename Proj = std::identity>
+
+    }
+
+    template <typename I, typename S, typename Comp = std::less<>>
+    constexpr I tim_sort(I first, S last, Comp comp = {})
+    {
+        if (last - first < detail::MinSize)
+            return insertion_sort(std::move(first), std::move(last), std::move(comp));
+
+        auto iter = first; // 
+        std::vector stack{ iter };
+        stack.reserve(64);
+
+        // min length
+        const auto remaining = std::distance(iter, last);
+        const auto min_run = detail::min_run_length(remaining);
+        do 
+        {
+            auto next_iter = detail::count_run_and_make_ascending(iter, last, comp);
+            if (next_iter - iter < min_run)
+            {
+                const auto dist = last - iter;
+                auto force = std::min(dist, min_run);
+                next_iter = insertion_sort(iter, iter + force, comp);
+            }
+            stack.emplace_back(next_iter);
+            detail::merge_collapse(stack, comp);
+            iter = next_iter;
+
+        } while (iter != last);
+
+        detail::merge_force_collapse(stack, comp);
+
+        return iter;
+    }
+
+}
+
+
+namespace leviathan
+{
+
+    struct insertion_sort_fn
+    {
+        // for simplifier, use random_access_iterator
+        template <std::random_access_iterator I, std::sentinel_for<I> S, typename Comp = std::ranges::less, typename Proj = std::identity>
         requires std::sortable<I, Comp, Proj>
         constexpr I operator()(I first, S last, Comp comp = {}, Proj proj = {}) const
         {
-            if (last - first < MinSize)
-                return insertion_sort(std::move(first), std::move(last), std::move(comp), std::move(proj));
-
-
-            auto iter = first; // 
-            std::vector stack{ iter };
-            stack.reserve(64);
-
-            // min length
-            const auto remaining = std::ranges::distance(iter, last);
-            const auto min_run = min_run_length(remaining);
-            do 
-            {
-                auto next_iter = count_run_and_make_ascending(iter, last, comp, proj);
-                if (next_iter - iter < min_run)
-                {
-                    const auto dist = last - iter;
-                    auto force = std::min(dist, min_run);
-                    next_iter = insertion_sort(iter, iter + force, comp, proj);
-                }
-                stack.emplace_back(next_iter);
-                merge_collapse(stack, comp, proj);
-                iter = next_iter;
-
-            } while (iter != last);
-
-            merge_force_collapse(stack, comp, proj);
-
-            return iter;
+            return sort::insertion_sort(std::move(first), std::move(last), sort::make_comp_proj(comp, proj));
         }
 
         template <std::ranges::random_access_range Range, typename Comp = std::ranges::less, typename Proj = std::identity>
         requires std::sortable<std::ranges::iterator_t<Range>, Comp, Proj>
         constexpr std::ranges::borrowed_iterator_t<Range>
-        operator()(Range&& r, Comp comp = {}, Proj proj = {}) const
+        operator()(Range &&r, Comp comp = {}, Proj proj = {}) const
         {
             return (*this)(std::ranges::begin(r), std::ranges::end(r), std::move(comp), std::move(proj));
         }
-
     };
 
-    inline constexpr tim_sort_fn tim_sort{ };
+    inline constexpr insertion_sort_fn insertion_sort{};
 
-/*
+    struct merge_sort_fn
+    {
+        template <std::random_access_iterator I, std::sentinel_for<I> S, typename Comp = std::ranges::less, typename Proj = std::identity>
+        requires std::sortable<I, Comp, Proj>
+        constexpr I operator()(I first, S last, Comp comp = {}, Proj proj = {}) const
+        {
+            return sort::merge_sort(std::move(first), std::move(last), sort::make_comp_proj(comp, proj));
+        }
 
-template <typename T>
-auto map(T fn) {
-    return [=](auto reduce_fn) {
-        return [=] (auto accum, auto input) {
-            return reduce_fn(accum, fn(input));
+        template <std::ranges::random_access_range Range, typename Comp = std::ranges::less, typename Proj = std::identity>
+        requires std::sortable<std::ranges::iterator_t<Range>, Comp, Proj>
+        constexpr std::ranges::borrowed_iterator_t<Range> operator()(Range &&r, Comp comp = {}, Proj proj = {}) const
+        {
+            return (*this)(std::ranges::begin(r), std::ranges::end(r), std::move(comp), std::move(proj));
+        }
+    };
+
+    inline constexpr merge_sort_fn merge_sort{};
+
+    // http://cr.openjdk.java.net/~martin/webrevs/jdk7/timsort/raw_files/new/src/share/classes/java/util/TimSort.java
+    struct tim_sort_fn
+    {
+        
+        template <std::random_access_iterator I, std::sentinel_for<I> S, typename Comp = std::ranges::less, typename Proj = std::identity>
+        requires std::sortable<I, Comp, Proj>
+        constexpr I operator()(I first, S last, Comp comp = {}, Proj proj = {}) const
+        {
+            return sort::tim_sort(std::move(first), std::move(last), sort::make_comp_proj(comp, proj));
+        }
+
+        template <std::ranges::random_access_range Range, typename Comp = std::ranges::less, typename Proj = std::identity>
+        requires std::sortable<std::ranges::iterator_t<Range>, Comp, Proj>
+        constexpr std::ranges::borrowed_iterator_t<Range>
+        operator()(Range &&r, Comp comp = {}, Proj proj = {}) const
+        {
+            return (*this)(std::ranges::begin(r), std::ranges::end(r), std::move(comp), std::move(proj));
+        }
+    };
+
+    inline constexpr tim_sort_fn tim_sort{};
+
+    // std::sort -> introsort
+    // Musser, D.: Introspective sorting and selection algorithms. Software Practice and Experience 27, 983–993 (1997)
+
+    /*
+
+    template <typename T>
+    auto map(T fn) {
+        return [=](auto reduce_fn) {
+            return [=] (auto accum, auto input) {
+                return reduce_fn(accum, fn(input));
+            };
         };
-    };
-}
+    }
 
-template <typename T>
-auto filter(T predicate) {
-    return [=](auto reduce_fn) {
-        return [=](auto accume, auto input) {
-            if (predicate(input)) {
-                return reduce_fn(accume, input);
-            } else {
-                return accume;
-            }
+    template <typename T>
+    auto filter(T predicate) {
+        return [=](auto reduce_fn) {
+            return [=](auto accume, auto input) {
+                if (predicate(input)) {
+                    return reduce_fn(accume, input);
+                } else {
+                    return accume;
+                }
+            };
         };
-    };
-}
+    }
 
 
-    std::istream_iterator<int> it{ std::cin };
-    std::istream_iterator<int> end_it;
-    auto even = [](int x) { return ~x & 1; };
-    auto twice = [](int x) { return x << 1; };
-    auto copy_and_advance = [](auto iter, auto input) {
-        *iter = input;
-        return ++iter;
-    };
-	// very ugly
-    std::accumulate(it, end_it, std::ostream_iterator<int>{ std::cout, ", " }, 
-        filter(even) (
-            map(twice) (
-                copy_and_advance
-            )
-        ));
-    std::cout << std::endl;
-*/
+        std::istream_iterator<int> it{ std::cin };
+        std::istream_iterator<int> end_it;
+        auto even = [](int x) { return ~x & 1; };
+        auto twice = [](int x) { return x << 1; };
+        auto copy_and_advance = [](auto iter, auto input) {
+            *iter = input;
+            return ++iter;
+        };
+        // very ugly
+        std::accumulate(it, end_it, std::ostream_iterator<int>{ std::cout, ", " },
+            filter(even) (
+                map(twice) (
+                    copy_and_advance
+                )
+            ));
+        std::cout << std::endl;
+    */
 
-}  // namespace leviathan
+} // namespace leviathan
 
 #endif
