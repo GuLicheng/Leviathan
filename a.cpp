2ad8b989ec5332589cb005b8e636437069cb3c3f
GuLicheng <576356467@qq.com>
2022-06-29 00:39:46 +0800

    Update

--- a.cpp
+++ a.cpp
@@ -1,11 +1,16 @@
 #include <type_traits>
-
+#include <iostream>
 #include <memory>
+#include "test/except_allocator.hpp"
 
 int main()
 {
-    std::allocator<int> alloc;
-    int val;
-    std::uninitialized_construct_using_allocator(&val, alloc, 3);
+    RecordPolymorphicAllocator<int> alloc;
+    double val;
+    double* p;
+    p = std::uninitialized_construct_using_allocator(&val, alloc, 3.5);
+    val = std::make_obj_using_allocator<double>(alloc, 3.14);
     std::cout << val << '\n';
+    std::cout << p << '\n';
+    Report();
 }
