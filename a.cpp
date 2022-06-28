d0204c648d8a9d7fad35a447e008d8a3eca1b38d
GuLicheng <576356467@qq.com>
2022-06-28 22:23:16 +0800

    Update

--- a.cpp
+++ a.cpp
@@ -1,3 +1,11 @@
 #include <type_traits>
 
+#include <memory>
 
+int main()
+{
+    std::allocator<int> alloc;
+    int val;
+    std::uninitialized_construct_using_allocator(&val, alloc, 3);
+    std::cout << val << '\n';
+}
