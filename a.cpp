e8526831012f6c6c18054b6fcbb5469d995a1b00
GuLicheng <576356467@qq.com>
2022-07-21 00:33:45 +0800

    Update

--- a.cpp
+++ a.cpp
@@ -2,9 +2,13 @@
 #include <set>
 #include <map>
 #include <lv_cpp/collections/internal/avl_tree.hpp>
+#include <string>
 
 int main()
 {
+    ::leviathan::collections::avl_set<std::string> as;
+
+    as.begin()->push_back('a');
 
 }
 
