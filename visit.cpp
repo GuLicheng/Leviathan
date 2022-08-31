680b9da8cadd15443193da1f6d020d7f1d387b26
GuLicheng <576356467@qq.com>
2022-08-31 19:50:03 +0800

    Update

--- visit.cpp
+++ visit.cpp
@@ -99,13 +99,22 @@ constexpr auto DoVisit(type_list<IndexSeq...> indices, Visitor&& visitor, Vs&&..
 }
 
 template <typename Visitor, typename V1, typename... Vs>
-constexpr auto Visit(Visitor&& visitor, V1&& v1, Vs&&... vs)
+constexpr auto VisitMulti(Visitor&& visitor, V1&& v1, Vs&&... vs)
 {
     constexpr auto first_size = std::variant_size_v<std::remove_cvref_t<V1>>;
     using first_index_seq = decltype(std::make_index_sequence<first_size>());
     using first_list = typename generate_index_sequence_list_by_index_sequence<first_index_seq>::type;
     using T = typename merge<first_list, std::make_index_sequence<std::variant_size_v<std::remove_cvref_t<Vs>>>...>::type;
     return DoVisit(T{}, (Visitor&&) visitor, (V1&&) v1, (Vs&&) vs...);
+}
+
+template <typename Visitor, typename... Vs>
+constexpr auto Visit(Visitor&& visitor, Vs&&... vs)
+{
+    if constexpr (sizeof...(Vs) == 0)
+        return ((Visitor&&) visitor)();
+    else
+        return VisitMulti((Visitor&&) visitor, (Vs&&) vs...);
 }
 
 // Source: https://en.cppreference.com/w/cpp/utility/variant/visit
@@ -120,7 +129,6 @@ using example_variant = std::variant<T0, T1>;
 // https://pbackus.github.io/blog/beating-stdvisit-without-really-trying.html
 // GCC 12.1 use `if constexpr` to optimize the case of sizeof...(Vs) == 1, so
 // we use two arguments to test whether compiler is able to inline the individual visitor functions.
-// https://godbolt.org/z/MWo5n9a9h
 int do_visit(example_variant v1, example_variant v2)
 {
 	return std::visit(overloaded {
