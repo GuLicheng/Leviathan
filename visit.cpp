c73c00d563ece9334b1e1fbaf09fd2f1e2f5b154
GuLicheng <576356467@qq.com>
2022-08-31 19:41:27 +0800

    Update

--- visit.cpp
+++ visit.cpp
@@ -1,8 +1,11 @@
+#include <assert.h>
 #include <iostream>
 #include <functional>
 #include <utility>
 #include <tuple>
 #include <variant>
+#include <iostream>
+// #include <lv_cpp/meta/template_info.hpp>
 
 /*
     For variant<int, double>, variant<int, double, bool>, we generate follow indices:
@@ -60,25 +63,27 @@ struct merge<T1, T2> : std::type_identity<typename merge_index_sequence<T1, T2>:
 template <typename T1, typename T2, typename T3, typename... Ts> 
 struct merge<T1, T2, T3, Ts...> : merge<typename merge<T1, T2>::type, T3, Ts...> { }; 
 
-// https://pbackus.github.io/blog/beating-stdvisit-without-really-trying.html
-// we use index_sequence, complier is able to inline the individual visitor functions(maybe...)
+template <size_t I, typename Variant>
+struct check_variant_return
+{
+    using type = std::conditional_t<std::is_lvalue_reference_v<Variant>,
+        std::add_lvalue_reference_t<std::variant_alternative_t<I, std::remove_reference_t<Variant>>>,
+        std::add_rvalue_reference_t<std::variant_alternative_t<I, std::remove_reference_t<Variant>>>>;
+};
+
+
 template <size_t I, size_t... Idx, typename... IndexSeq, typename Visitor, typename... Vs>
 constexpr auto DoVisitImpl(type_list<IndexSeq...> _, std::index_sequence<Idx...>, Visitor&& visitor, Vs&&... vs)
 {
     const std::array indices = { vs.index()... };
     constexpr std::array cur_indices = { Idx... };
-    
+        
     if constexpr (I + 1 == sizeof...(IndexSeq))
-        if (indices == cur_indices)
-            return std::invoke((Visitor&&) visitor, std::get<Idx>((Vs&&)vs) ...);
-        else
-            throw std::bad_variant_access{}; // variant.index() may equal to std::variant::npos
+        return std::invoke((Visitor&&) visitor, (typename check_variant_return<Idx, Vs>::type)*std::get_if<Idx>(std::addressof(vs)) ...);
     else
     {
-        if (indices == cur_indices)
-            // use follow version can generate less asm code.
-            // return std::invoke((Visitor&&) visitor, std::__detail::__variant::__get<Idx>((Vs&&)vs) ...);
-            return std::invoke((Visitor&&) visitor, std::get<Idx>((Vs&&)vs) ...);
+        if (indices == cur_indices) 
+            return std::invoke((Visitor&&) visitor, (typename check_variant_return<Idx, Vs>::type)*std::get_if<Idx>(std::addressof(vs)) ...);
         else
             return DoVisitImpl<I + 1>(_, std::tuple_element_t<I + 1, std::tuple<IndexSeq...>>(), (Visitor&&) visitor, (Vs&&)vs...);
     }
@@ -87,90 +92,67 @@ constexpr auto DoVisitImpl(type_list<IndexSeq...> _, std::index_sequence<Idx...>
 template <typename... IndexSeq, typename Visitor, typename... Vs>
 constexpr auto DoVisit(type_list<IndexSeq...> indices, Visitor&& visitor, Vs&&... vs)
 {
+    if ((std::forward<Vs>(vs).valueless_by_exception() || ...))
+        throw std::bad_variant_access{};
+
     return DoVisitImpl<0>(indices, std::tuple_element_t<0, std::tuple<IndexSeq...>>(), (Visitor&&) visitor, (Vs&&) vs...);
 }
 
 template <typename Visitor, typename V1, typename... Vs>
 constexpr auto Visit(Visitor&& visitor, V1&& v1, Vs&&... vs)
 {
-    constexpr static auto first_size = std::variant_size_v<std::remove_cvref_t<V1>>;
+    constexpr auto first_size = std::variant_size_v<std::remove_cvref_t<V1>>;
     using first_index_seq = decltype(std::make_index_sequence<first_size>());
     using first_list = typename generate_index_sequence_list_by_index_sequence<first_index_seq>::type;
     using T = typename merge<first_list, std::make_index_sequence<std::variant_size_v<std::remove_cvref_t<Vs>>>...>::type;
     return DoVisit(T{}, (Visitor&&) visitor, (V1&&) v1, (Vs&&) vs...);
 }
 
+// Source: https://en.cppreference.com/w/cpp/utility/variant/visit
+template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
-// tuple_cat
-// FIXME: 
-// 1. rvalue_reference is not permitted
-// 2. automatically unfold tuple-like type, but only one level, why not recursive ?
-template <size_t I, typename IndexSeq> struct generate_indices_for_one_tuple;
-
-template <size_t I, size_t... Idx> 
-struct generate_indices_for_one_tuple<I, std::index_sequence<Idx...>>
-    : std::type_identity<type_list<std::index_sequence<I, Idx>...>> { };
-
-template <typename IndexSeq, typename Tuples> struct generate_indices_for_tuples;
-
-// (0, 1)  { (0, 1) (0, 2) }
-template <size_t... Idx, typename... IndexSeq> 
-struct generate_indices_for_tuples<std::index_sequence<Idx...>, type_list<IndexSeq...>>
-    : std::type_identity<type_list<typename generate_indices_for_one_tuple<Idx, IndexSeq>::type...>> { };
+struct T0 {};
+struct T1 {};
 
 
-template <typename... IndexSeq, typename Tuple>
-auto TupleCatImpl(type_list<IndexSeq...>, Tuple&& tuple)
+using example_variant = std::variant<T0, T1>;
+// https://pbackus.github.io/blog/beating-stdvisit-without-really-trying.html
+// GCC 12.1 use `if constexpr` to optimize the case of sizeof...(Vs) == 1, so
+// we use two arguments to test whether compiler is able to inline the individual visitor functions.
+// https://godbolt.org/z/MWo5n9a9h
+int do_visit(example_variant v1, example_variant v2)
 {
-    auto get_element = [&]<size_t OuterIndex, size_t InnerIndex>(std::index_sequence<OuterIndex, InnerIndex>) {
-        return std::get<InnerIndex>(std::get<OuterIndex>(tuple));
-    };
-    return std::make_tuple(get_element(IndexSeq{})...);
+	return std::visit(overloaded {
+	// return Visit(overloaded {
+		[](T0 , T0) { return 3; },
+		[](T1,  T0) { return 5; },
+		[](T0,  T1) { return 8; },
+		[](T1,  T1) { return 13; },
+	}, v1, v2);
 }
 
-template <typename... Tuples>
-auto TupleCat(Tuples&&... tuples)
-{
-    using T1 = decltype(std::make_index_sequence<sizeof...(Tuples)>());
-    using T2 = type_list<decltype(std::make_index_sequence<std::tuple_size_v<std::remove_cvref_t<Tuples>>>())...>;
-    using T3 = typename generate_indices_for_tuples<T1, T2>::type;
-    using T4 = typename flatten<type_list, T3>::type;
-    return TupleCatImpl(T4{}, std::forward_as_tuple((Tuples&&)tuples...));
-}
+// void test_variant_visit()
+// {
+//     const std::variant<int, double> v1 = 1;
+//     std::variant<int, std::string> v2 = std::string("Hello");
+//     auto f = [](auto x, auto y) -> int {
+//         if constexpr (std::is_same_v<decltype(y), std::string>)
+//             return x + y.size();
+//         else
+//             return x + y;
+//     };
 
-#include <iostream>
-#include <lv_cpp/meta/template_info.hpp>
+//     auto result = Visit(f, v1, std::move(v2));
 
-void test_variant_visit()
-{
-    std::variant<int, double> v1 = 1;
-    std::variant<int, std::string> v2 = std::string("Hello");
-    auto f = [](auto x, auto y) -> int {
-        if constexpr (std::is_same_v<decltype(y), std::string>)
-            return x + y.size();
-        else
-            return x + y;
-    };
+//     std::cout << result << '\n';
+// }
 
-    auto result = Visit(f, v1, v2);
-    std::cout << result << '\n';
-}
 
-void test_tuple_cat()
-{
-    auto ret = TupleCat(std::make_tuple(1, 2.0), std::make_tuple(true, std::string("Hello")));
-    PrintTypeCategory(ret);
-}
 
 int main()
 {
-    // test_tuple_cat();
-    int a = 1;
-
-    std::tuple<int, int&, const int> t{ 1, a, 0 };
-    std::tuple<int> t1{ 0 };
-    auto ret = std::tuple_cat(t, t1, std::make_tuple(1, std::make_tuple(1, 2, 3)));
-    PrintTypeCategory(ret);
+    // test_variant_visit();
 }
 
 
