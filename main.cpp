a128613b6a1aa1c9102055f0cf6d8e6b351f9b88
GuLicheng <576356467@qq.com>
2024-04-27 21:40:24 +0800

    Update^_^

--- main.cpp
+++ main.cpp
@@ -6,34 +6,9 @@
 #include <bitset>
 #include <array>
 
-    using leviathan::math::int128_t;
-    using leviathan::math::uint128_t;
-
-    template <typename T>
-    int128_t make_int128_from_float1(T v)
-    {
-        uint128_t result = v < 0 ? -uint128_t(-v) : uint128_t(v);
-        return static_cast<int128_t>(result);
-    }
-
-    template <typename T>
-    int128_t make_int128_from_float2(T v)
-    {
-        uint128_t result = std::signbit(v) ? -uint128_t(-v) : uint128_t(v);
-        return static_cast<int128_t>(result);
-    }
 
 int main(int argc, char const *argv[])
 {
-    std::puts(make_int128_from_float1(+0.0).to_string().c_str());
-    std::puts(make_int128_from_float1(-0.0).to_string().c_str());
-
-    std::puts(make_int128_from_float2(+0.0).to_string().c_str());
-    std::puts(make_int128_from_float2(-0.0).to_string().c_str());
-
-    int x = 0;
-
-    leviathan::math::signbit(x);
 
     return 0;
 }
