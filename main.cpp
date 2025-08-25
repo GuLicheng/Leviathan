c4fb5f6d6d6a6cbf49e2a85c86db9a0f7f195ee7
GuLicheng <576356467@qq.com>
2025-08-26 00:19:38 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,45 +1,49 @@
 #include <experimental/nom/nom.hpp>
 #include <experimental/nom/parser.hpp>
 #include <leviathan/type_caster.hpp>
+#include <print>
 
 
 // [section]
 
+auto left_bracket = nom::sequence::preceded(
+    nom::character::space0,
+    nom::character::char_('[')
+);
+
+auto right_bracket = nom::sequence::terminated(
+    nom::character::char_(']'),
+    nom::character::space0
+);
+
+auto identifier = nom::sequence::delimited(
+    nom::character::space0,
+    nom::character::alphanumeric1,
+    nom::character::space0
+);
+
 auto section_parser = nom::sequence::delimited(
-    nom::sequence::preceded(
-        nom::character::space0,
-        nom::character::char_('[')
-    ),
-    nom::sequence::delimited(
-        nom::character::space0,
-        nom::branch::alt(
-            nom::character::alphanumeric1,
-            nom::character::one_of(".")
-        ),
-        nom::character::space0
-    ),
-    nom::sequence::terminated(
-        nom::character::char_(']'),
-        nom::character::space0
-    )
+    left_bracket,
+    identifier,
+    right_bracket
 );
 
 int main(int argc, char const *argv[])
 {
 
-    std::string_view context = " [ Boris.Weapon.Anno ] ";
+    std::string_view context = " [ Boris0Weapon0Anno ] ";
 
     auto result = section_parser(context);
 
-    // if (result)
-    // {
-    //     std::print("Parsed section: '{}'\n", *result);
-    //     std::print("Remaining context: '{}'\n", context);
-    // }
-    // else
-    // {
-    //     std::print("Failed to parse section: {}\n", result.error().info);
-    // }
+    if (result)
+    {
+        std::print("Parsed section: '{}'\n", *result);
+        std::print("Remaining context: '{}'\n", context);
+    }
+    else
+    {
+        std::print("Failed to parse section: {}\n", result.error().info);
+    }
 
     return 0;
 }
