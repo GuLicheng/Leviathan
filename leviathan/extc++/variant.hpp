e4d4103b44d415ee54c8c46354b03723f6dac5df
GuLicheng <576356467@qq.com>
2026-05-24 20:38:20 +0800

    Update

---
+++ leviathan/extc++/variant.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <format>
+#include <variant>
+
+namespace cpp
+{
+
+struct tag_union_formatter
+{
+    template <typename ParseContext>
+    constexpr typename ParseContext::iterator parse(ParseContext& ctx) 
+    { 
+        return ctx.begin();
+    }
+
+    template <typename Union, typename FmtContext>
+    typename FmtContext::iterator format(const Union& u, FmtContext& ctx) const
+    {
+        return std::visit([&ctx](const auto& value) {
+            return std::format_to(ctx.out(), "{}", value);
+        }, u);
+    }
+};
+
+}
+
+template <typename... Types>
+struct std::formatter<std::variant<Types...>> : cpp::tag_union_formatter { };
--- main.cpp
+++ main.cpp
@@ -5,86 +5,14 @@
 #include <leviathan/config_parser/json/json.hpp>
 #include <leviathan/extc++/format.hpp>
 #include <leviathan/extc++/enum.hpp>
+#include <leviathan/extc++/variant.hpp>
 #include <print>
-
-constexpr const char* JsonString = R"(
-
-{
-    "X": 42,
-    "name": "Test",
-    "values": [1.0, 2.0, 3.0],
-    "color": "_RED_",
-    "bValue": true,
-    "UnnamedStruct": {
-        "i32": 100,
-        "f64": 3.14
-    }
-}
-
-)";
-
-enum struct 
-[[=cpp::derive::from<cpp::json::value>]] 
-[[=cpp::derive::debug]]
-Color
-{
-    Red [[=cpp::refl::rename("_RED_")]],
-    Blue,
-    Green
-};
-
-struct 
-[[=cpp::derive::debug]]
-[[=cpp::derive::from<cpp::json::value>]] 
-Base
-{
-    [[=cpp::refl::uppercase]]
-    int x;
-
-    [[=cpp::refl::lowercase]]
-    std::string NAME;
-
-    std::vector<double> values;
-
-    Color color;
-};
-
-struct 
-[[=cpp::derive::from<cpp::json::value>]]
-[[=cpp::refl::pascal_case]] 
-[[=cpp::derive::debug]]
-Derived : Base
-{
-    bool bValue;
-
-    struct [[=cpp::derive::debug, =cpp::derive::from<cpp::json::value>]] {
-        int i32;
-        double f64;
-    } unnamed_struct; 
-
-};
-
-void TestAnnotation()
-{
-    auto j = cpp::json::loads(JsonString);
-    auto d = cpp::cast<Derived>(j);
-
-    std::println("{}", d.x);
-    std::println("{}", d.NAME);
-    std::println("{}", d.values);
-    std::println("{}", d.color);
-    std::println("{}", d.bValue);
-    std::println("{}", d.unnamed_struct.i32);
-    std::println("{}", d.unnamed_struct.f64);
-
-    auto p = cpp::json::load(R"(D:\Library\Leviathan\test.json)");
-    auto s = p["Name"].as<std::string>();
-    std::println("{} - {}", s, s.size());
-}
-
 
 
 int main()
 {
+    std::variant<int, std::string> v = "Hello, World!";
+    println("Variant holds: {}", v);
+
     return 0;
 }
