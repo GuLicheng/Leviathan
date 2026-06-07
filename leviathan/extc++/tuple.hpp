53b37cf190e7ad82768c3bf41ffca934c64e645c
master <576356467@qq.com>
2026-06-07 20:25:18 +0800

    Update

--- leviathan/extc++/tuple.hpp
+++ leviathan/extc++/tuple.hpp
@@ -1,11 +1,83 @@
 #pragma once
 
-#include <leviathan/extc++/concepts.hpp>
-#include <leviathan/extc++/hash.hpp>
+#include <functional>
+#include <ranges>
+#include <meta>
 
 namespace cpp
 {
-    
+
+namespace detail
+{
+
+template <typename... Ts>
+consteval auto define_basic_tuple()
+{
+    struct storage;
+
+    struct int2string
+    {
+        static constexpr std::string operator()(std::size_t index) 
+        {
+            std::string result;
+
+            do 
+            {
+                result.push_back('0' + (index % 10));
+                index /= 10;
+            } while (index > 0);
+            result += '_';
+
+            return std::string(result.rbegin(), result.rend());
+        }
+    };
+
+    consteval
+    {
+        auto member_specs = std::vector<std::meta::info>{ ^^Ts... } 
+                          | std::views::enumerate 
+                          | std::views::transform([](auto indexed_info) {
+                                auto [index, info] = indexed_info;
+                                return data_member_spec(info, { .name = int2string{}(index), .no_unique_address = true }); 
+                            });
+
+        std::meta::define_aggregate(^^storage, member_specs);
+    }
+
+    return ^^storage;
+}
+
+template <typename... Ts>
+using basic_tuple = typename [:define_basic_tuple<Ts...>():];
+
+}  // namespace detail
+
+template <typename... Ts>
+struct tuple : detail::basic_tuple<Ts...>
+{
+    using base = detail::basic_tuple<Ts...>;
+
+    template <typename... Args>
+    constexpr tuple(Args&&... args) : base((Args&&)args...) {}
+
+};
+
+template <typename... Ts>
+tuple(Ts&&...) -> tuple<Ts...>;
+
+template <typename Fn, typename TupleLike>
+constexpr auto apply(Fn&& fn, TupleLike&& tl)
+{
+    auto&& [...elements] = (TupleLike&&)tl;
+    return std::invoke((Fn&&)fn, (decltype(elements)&&)elements...);
+}
+
+template <typename... Args>
+constexpr auto make_tuple(Args&&... args)
+{
+    return tuple<std::unwrap_ref_decay_t<Args>...>((Args&&)args...);
+}
+
 
 } // namespace cpp
 
--- main.cpp
+++ main.cpp
@@ -9,59 +9,17 @@
 #include <utility>
 #include <leviathan/annotations/all.hpp>
 #include <leviathan/extc++/format.hpp>
+#include <leviathan/extc++/tuple.hpp>
 #include "nlohmann.hpp"
 
-inline constexpr struct { } Fact;
-inline constexpr struct { } Theory;
-inline constexpr struct { } ParameterAnnotation;
-
-struct [[=ParameterAnnotation, =cpp::derive::debug]] MyParameter
-{
-    [[=cpp::refl::rename("X")]]
-    int x;
-    double y;
-    const char* z;
-};
-
-namespace N
-{
-    [[=Fact]]
-    void fun1() { std::println("N::fun1 called"); }
-
-    [[=Theory]]
-    [[=MyParameter{.x = 1, .y = 3.14, .z = "hello"}]]
-    void fun2(int x, double y, const char* z) 
-    {
-        std::println("N::fun2 called with x={}, y={}, z={}", x, y, z);
-    }
-};
-
-// void show()
-// {
-//     template for (constexpr auto m : define_static_array(members_of(^^N, std::meta::access_context::current())))
-//     {
-//         if constexpr (cpp::refl::has_annotation(m, Fact)) 
-//         {
-//             std::invoke([:m:]);
-//         }
-//         else if constexpr (cpp::refl::has_annotation(m, Theory)) 
-//         {
-//             template for (constexpr auto a : define_static_array(annotations_of(m))) 
-//             {
-//                 if constexpr (cpp::refl::has_annotation(type_of(a), ParameterAnnotation)) 
-//                 {
-//                     constexpr auto [...params] = extract<typename [:type_of(a):]>(a);
-//                     std::invoke([:m:], params...);
-//                 }
-//             }
-//         }
-//     }
-// }
 
 int main(int argc, char const *argv[]) 
 {
 
+    cpp::tuple<int, double> t(42, 3.14);
+    std::println("Tuple: ({}, {})", t._0, t._1);
 
+    std::println("Hello, Leviathan Annotations!");
 
     return 0;
 }
