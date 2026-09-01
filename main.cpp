e3a7c675a4abed8bb6a1628c43894edb7a303eb6
master <576356467@qq.com>
2026-09-01 23:45:39 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,59 +1,99 @@
 #include <iostream>
 #include <functional>
 
-template <typename Derived>
-struct AdaptorClosure
+template <typename Adaptor, typename... Args>
+concept adaptor_invocable = requires 
 {
-    template<typename L, typename R>
-    friend constexpr auto operator|(L&& lhs, R&& rhs);
-
-    template<typename Self, typename Other>
-	friend constexpr auto operator|(Other&& lhs, Self&& rhs);
+    std::declval<Adaptor>()(std::declval<Args>()...);
 };
 
-template <typename L, typename R>
-struct Pipe : AdaptorClosure<Pipe<L, R>>
+template <typename Adaptor, typename... Args> struct partial;
+
+template <typename Lhs, typename Rhs> struct pipe;
+
+// simple pipeline
+struct range_adaptor_closure
 {
-    [[no_unique_address]] L lhs;
-    [[no_unique_address]] R rhs;
+    template <typename Self, typename Range>
+    requires std::derived_from<std::remove_cvref_t<Self>, range_adaptor_closure> && adaptor_invocable<Self, Range>
+    friend constexpr auto operator|(Range&& r, Self&& self)
+    { return std::forward<Self>(self)(std::forward<Range>(r)); }
 
-    template <typename T, typename U>
-    constexpr Pipe(T&& lhs, U&& rhs) : lhs(std::forward<T>(lhs)), rhs(std::forward<U>(rhs)) {}
+    template <typename Lhs, typename Rhs>
+    requires std::derived_from<Lhs, range_adaptor_closure> && std::derived_from<Rhs, range_adaptor_closure>
+    friend constexpr auto operator|(Lhs lhs, Rhs rhs)
+    { return pipe<Lhs, Rhs>{ std::move(lhs), std::move(rhs)}; }
+};
 
-    template <typename Self, typename Other>
-    constexpr auto operator()(this Self&& self, Other&& other) const
-    {
-        return std::invoke(
-            std::forward_like<Self>(self.rhs),
-            std::invoke(
-                std::forward_like<Self>(self.lhs),
-                std::forward<Other>(other)
-            )   
-        );
-    }
-
+template <typename Derived>
+struct range_adaptor
+{
+    template <typename... Args> 
+    // requires adaptor_invocable<Derived, Args...>
+    constexpr auto operator()(Args&&... args) const 
+    { return partial<Derived, std::decay_t<Args>...>{ std::forward<Args>(args)... }; }
 };
 
 template <typename Adaptor, typename... Args>
-struct Partial : AdaptorClosure<Partial<Adaptor, Args...>>
+struct partial : range_adaptor_closure
 {
-    using binder = decltype(std::bind_back(std::declval<Adaptor>(), std::declval<Args>()...));
+    std::tuple<Args...> m_args;
 
-    [[no_unique_address]] binder m_binder;
+    constexpr partial(Args... args) : m_args(std::move(args)...) { }
 
-    template <typename... Ts>
-    constexpr Partial(Ts&&... args) 
-        : m_binder(std::bind_back(Adaptor(), std::forward<Ts>(args)...)) { }
-    
-    template <typename Self, typename Other>
-    constexpr auto operator()(this Self&& self, Other&& other) 
+    template <typename Range>
+    requires adaptor_invocable<Adaptor, Range, const Args&...>
+    constexpr auto operator()(Range&& r) const&
     {
-        return std::invoke(
-            std::forward_like<Self>(self.m_binder),
-            std::forward<Other>(other)
-        );
+        auto forwarder = [&r](const auto&... args) {
+            return Adaptor{}(std::forward<Range>(r), args...);
+        };
+        return std::apply(forwarder, m_args);
     }
+
+    template <typename Range>
+    requires adaptor_invocable<Adaptor, Range, Args...>
+    constexpr auto operator()(Range&& r) &&
+    {
+        auto forwarder = [&r](auto&... args) {
+            return Adaptor{}(std::forward<Range>(r), std::move(args)...);
+        };
+        return std::apply(forwarder, m_args);
+    }
+
+    template <typename Range>
+    constexpr auto operator()(Range&& r) const&& = delete;
 };
+
+template <typename Lhs, typename Rhs, typename Range>
+concept pipe_invocable = requires 
+{
+    std::declval<Rhs>()(std::forward<Lhs>()(std::declval<Range>()));
+};
+
+template <typename Lhs, typename Rhs>
+struct pipe : range_adaptor_closure
+{
+    [[no_unique_address]] Lhs m_lhs;
+    [[no_unique_address]] Rhs m_rhs;
+
+    constexpr pipe(Lhs lhs, Rhs rhs) : m_lhs(std::move(lhs)), m_rhs(std::move(rhs)) { }
+
+    template <typename Range>
+    requires pipe_invocable<const Lhs&, const Rhs&, Range>
+    constexpr auto operator()(Range&& r) const&
+    { return m_rhs(m_lhs(std::forward<Range>(r))); }
+
+    template <typename Range>
+    requires pipe_invocable<Lhs, Rhs, Range>
+    constexpr auto operator()(Range&& r) &&
+    { return std::move(m_rhs)(std::move(m_lhs)(std::forward<Range>(r))); }
+
+    template <typename Range>
+    constexpr auto operator()(Range&& r) const&& = delete;
+
+};
+
 
 
 int main(int argc, char const *argv[])
