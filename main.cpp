43ebfdd83cb8fcb1c5476b0a1bc3c1faf7331c01
master <576356467@qq.com>
2026-09-04 00:12:24 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <functional>
+#include <ranges>
+
+template <typename Adaptor, typename... Args> struct partial;
+
+template <typename Lhs, typename Rhs> struct pipe;
 
 template <typename Adaptor, typename... Args>
 concept adaptor_invocable = requires 
@@ -7,31 +12,33 @@ concept adaptor_invocable = requires
     std::declval<Adaptor>()(std::declval<Args>()...);
 };
 
-template <typename Adaptor, typename... Args> struct partial;
-
-template <typename Lhs, typename Rhs> struct pipe;
+template <typename Derived, typename Base>
+concept remove_cvref_derived_from = std::derived_from<std::remove_cvref_t<Derived>, Base>;
 
 // simple pipeline
 struct range_adaptor_closure
 {
-    template <typename Self, typename Range>
-    requires std::derived_from<std::remove_cvref_t<Self>, range_adaptor_closure> && adaptor_invocable<Self, Range>
+    template <remove_cvref_derived_from Self, typename Range>
+        requires adaptor_invocable<Self, Range>
     friend constexpr auto operator|(Range&& r, Self&& self)
-    { return std::forward<Self>(self)(std::forward<Range>(r)); }
+    { 
+        return std::forward<Self>(self)(std::forward<Range>(r)); 
+    }
 
-    template <typename Lhs, typename Rhs>
-    requires std::derived_from<Lhs, range_adaptor_closure> && std::derived_from<Rhs, range_adaptor_closure>
-    friend constexpr auto operator|(Lhs lhs, Rhs rhs)
-    { return pipe<Lhs, Rhs>{ std::move(lhs), std::move(rhs)}; }
+    template <remove_cvref_derived_from<range_adaptor_closure> Lhs, remove_cvref_derived_from<range_adaptor_closure> Rhs>
+    friend constexpr auto operator|(Lhs&& lhs, Rhs&& rhs)
+    { 
+        return pipe<std::decay_t<Lhs>, std::decay_t<Rhs>>{ std::forward<Lhs>(lhs), std::forward<Rhs>(rhs)}; 
+    }
 };
 
 template <typename Derived>
 struct range_adaptor
 {
     template <typename... Args> 
-    // requires adaptor_invocable<Derived, Args...>
+        requires adaptor_invocable<Derived, Args...>
     constexpr auto operator()(Args&&... args) const 
-    { return partial<Derived, std::decay_t<Args>...>{ std::forward<Args>(args)... }; }
+    { return partial<Derived, std::decay_t<Args>...>{ 0, std::forward<Args>(args)... }; }
 };
 
 template <typename Adaptor, typename... Args>
@@ -39,30 +46,41 @@ struct partial : range_adaptor_closure
 {
     std::tuple<Args...> m_args;
 
-    constexpr partial(Args... args) : m_args(std::move(args)...) { }
+    template <typename... Ts>
+    constexpr partial(int, Ts&&... args) : m_args(std::forward<Ts>(args)...) { }
 
-    template <typename Range>
-    requires adaptor_invocable<Adaptor, Range, const Args&...>
-    constexpr auto operator()(Range&& r) const&
+    template <typename Self, typename Range>
+        requires adaptor_invocable<Adaptor, Range, const Args&...>
+    constexpr auto operator()(this Self&& self, Range&& r) const
     {
-        auto forwarder = [&r](const auto&... args) {
-            return Adaptor{}(std::forward<Range>(r), args...);
+        auto forwarder = [&r]<typename... Ts>(Ts&&... args) {
+            return Adaptor{}(std::forward<Range>(r), std::forward<Ts>(args)...);
         };
-        return std::apply(forwarder, m_args);
+        return std::apply(forwarder, std::forward_like<Self>(self.m_args));
     }
 
-    template <typename Range>
-    requires adaptor_invocable<Adaptor, Range, Args...>
-    constexpr auto operator()(Range&& r) &&
-    {
-        auto forwarder = [&r](auto&... args) {
-            return Adaptor{}(std::forward<Range>(r), std::move(args)...);
-        };
-        return std::apply(forwarder, m_args);
-    }
+    // template <typename Range>
+    // requires adaptor_invocable<Adaptor, Range, const Args&...>
+    // constexpr auto operator()(Range&& r) const&
+    // {
+    //     auto forwarder = [&r](const auto&... args) {
+    //         return Adaptor{}(std::forward<Range>(r), args...);
+    //     };
+    //     return std::apply(forwarder, m_args);
+    // }
 
-    template <typename Range>
-    constexpr auto operator()(Range&& r) const&& = delete;
+    // template <typename Range>
+    // requires adaptor_invocable<Adaptor, Range, Args...>
+    // constexpr auto operator()(Range&& r) &&
+    // {
+    //     auto forwarder = [&r](auto&... args) {
+    //         return Adaptor{}(std::forward<Range>(r), std::move(args)...);
+    //     };
+    //     return std::apply(forwarder, m_args);
+    // }
+
+    // template <typename Range>
+    // constexpr auto operator()(Range&& r) const&& = delete;
 };
 
 template <typename Lhs, typename Rhs, typename Range>
@@ -148,6 +166,5 @@ public:
 
 int main(int argc, char const *argv[])
 {
-    
     return 0;
 }
